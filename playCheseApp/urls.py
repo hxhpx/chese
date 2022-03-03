e2238134c945552613a4ef345ea509174b5e15c6
hxhpx <78634601+hxhpx@users.noreply.github.com>
2022-03-03 17:52:50 +0800

    Delete playCheseApp directory

--- playCheseApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-
-urlpatterns = [
-    url(r'make_size', views.make_size,),
-    url(r'getpiecearray', views.get_piece_array,),
-    url(r'restart',views.restart,)
-]
