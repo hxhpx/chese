bd1d12ba077f2867a3a92fa5c2ad2a08c02e2e37
hxhpx <78634601+hxhpx@users.noreply.github.com>
2022-03-03 17:59:40 +0800

    Add files via upload

---
+++ frontChese/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" style="font-size:30px; font-weight:bold">
+    Gamma Lab 棋盘室
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
